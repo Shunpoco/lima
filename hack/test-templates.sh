df37414651957e292d50fa56aa607c6c7b79b49d
Shunpoco <tkngsnsk313320@gmail.com>
2026-05-23 22:28:59 +0100

    test

--- hack/test-templates.sh
+++ hack/test-templates.sh
@@ -396,6 +396,8 @@ if [[ -n ${CHECKS["container-engine"]} ]]; then
 	fi
 	limactl shell "$NAME" $sudo $CONTAINER_ENGINE pull --quiet ${nginx_image}
 	limactl shell "$NAME" $sudo $CONTAINER_ENGINE run -d --name nginx -p 127.0.0.1:8080:80 ${nginx_image}
+    sleep 10
+    limactl shell "$NAME" $sudo $CONTAINER_ENGINE ps -a
 
 	timeout 3m bash -euxc "until curl -f --retry 30 --retry-connrefused http://127.0.0.1:8080; do sleep 3; done"
 
