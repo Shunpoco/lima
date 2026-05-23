a6d66e589080b439cd725ee6c38cb7452ab3139f
Shunpoco <tkngsnsk313320@gmail.com>
2026-05-24 00:37:46 +0100

    test4

--- hack/test-templates.sh
+++ hack/test-templates.sh
@@ -396,7 +396,10 @@ if [[ -n ${CHECKS["container-engine"]} ]]; then
 	fi
 	limactl shell "$NAME" $sudo $CONTAINER_ENGINE pull --quiet ${nginx_image}
 	limactl shell "$NAME" $sudo $CONTAINER_ENGINE run -d --name nginx -p 127.0.0.1:8080:80 ${nginx_image}
+	limactl shell "$NAME" sudo $CONTAINER_ENGINE run -d --name nginx2 -p 127.0.0.1:8081:80 ${nginx_image}
     sleep 10
+    limactl shell "$NAME" sudo lsof -i:8080
+    limactl shell "$NAME" sudo lsof -i:8081
     limactl shell "$NAME" $sudo $CONTAINER_ENGINE ps -a
     limactl shell "$NAME" $sudo $CONTAINER_ENGINE inspect nginx
     limactl shell "$NAME" $sudo $CONTAINER_ENGINE logs nginx
