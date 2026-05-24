94d96252000a0f7db0a0a17a073f79e6eefdc727
Shunpoco <tkngsnsk313320@gmail.com>
2026-05-24 09:27:12 +0100

    test7

--- hack/test-templates.sh
+++ hack/test-templates.sh
@@ -396,14 +396,13 @@ if [[ -n ${CHECKS["container-engine"]} ]]; then
 	fi
     # limactl shell "$NAME" sudo lsof -i:8080
     # limactl shell "$NAME" sudo lsof -i:8081
-    limactl shell "$NAME" ls /run/containerd/
+    limactl shell "$NAME" sudo /usr/local/bin/containerd --version
 	limactl shell "$NAME" $sudo $CONTAINER_ENGINE pull --quiet ${nginx_image}
 	limactl shell "$NAME" $sudo $CONTAINER_ENGINE run -d --name nginx -p 127.0.0.1:8080:80 ${nginx_image}
-    limactl shell "$NAME" ls /run/containerd/
-	limactl shell "$NAME" $sudo $CONTAINER_ENGINE run -d --name nginx2 -p 127.0.0.1:8081:80 ${nginx_image}
-    limactl shell "$NAME" ls /run/containerd/
+    limactl shell "$NAME" $sudo $CONTAINER_ENGINE ps -a
+	limactl shell "$NAME" $sudo $CONTAINER_ENGINE run -d --name nginx2 ${nginx_image}
+    limactl shell "$NAME" $sudo $CONTAINER_ENGINE ps -a
 	limactl shell "$NAME" sudo $CONTAINER_ENGINE run -d --name nginx3 -p 127.0.0.1:8082:80 ${nginx_image}
-    limactl shell "$NAME" ls /run/containerd/
     sleep 10
     limactl shell "$NAME" $sudo $CONTAINER_ENGINE ps -a
     limactl shell "$NAME" $sudo $CONTAINER_ENGINE inspect nginx
