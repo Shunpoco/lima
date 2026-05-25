dd65cf2036427d2b410dc79583358f524f06cdef
Shunpoco <tkngsnsk313320@gmail.com>
2026-05-25 15:40:26 +0100

    test19

--- hack/test-templates.sh
+++ hack/test-templates.sh
@@ -397,7 +397,7 @@ if [[ -n ${CHECKS["container-engine"]} ]]; then
     limactl shell "$NAME" sudo cat /var/log/cloud-init-output.log
     limactl shell "$NAME" sudo /usr/local/bin/containerd --version
     limactl shell "$NAME" systemctl --user status containerd.service
-	limactl shell "$NAME" $sudo $CONTAINER_ENGINE --debug-full pull --quiet ${nginx_image}
+	# limactl shell "$NAME" $sudo $CONTAINER_ENGINE --debug-full pull --quiet ${nginx_image}
 	# limactl shell --debug "$NAME" $sudo $CONTAINER_ENGINE run -d --name nginx -p 127.0.0.1:8080:80 ${nginx_image}
     # sleep 10
     # limactl shell --debug "$NAME" $sudo $CONTAINER_ENGINE  logs nginx
